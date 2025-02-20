d89c0afd9cc98c9bfd4cc84867fe9188b3fffd69
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-02-20 11:48:52 +0100

    Restaaaaart !

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,7 +5,7 @@ import "../styles/ProductCard.css";
 function ProductCard({produit}) {
     return (
         <div className="product-card">
-            <image src="public/ai-generated-7789179_1280.jpg"></image>
+            <img src="/ai-generated-7789179_1280.jpg"></img>
             <h3>{produit.designation_produit}</h3>
             <p>{produit.prix_ttc}€</p>
             <Link to={`/produit/${produit.id}`} className="details-btn">
