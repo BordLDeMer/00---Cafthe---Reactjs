477a7bbbf633708122c18b052ee6c9dd198ad595
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-02-20 11:30:13 +0100

    Restaaaaart !

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,7 +5,7 @@ import "../styles/ProductCard.css";
 function ProductCard({produit}) {
     return (
         <div className="product-card">
-            {/* image */}
+            <image src="public/ai-generated-7789179_1280.jpg"></image>
             <h3>{produit.designation_produit}</h3>
             <p>{produit.prix_ttc}€</p>
             <Link to={`/produit/${produit.id}`} className="details-btn">
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -14,7 +14,7 @@ function ProductList(props) {
   useEffect(() => {
     const fetchProduits = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/produits");
+        const response = await axios.get("http://localhost:3000/api/produitsstart");
         setProduits(response.data);
       } catch (error) {
         console.error("Erreur de chargement des produits ", error);
