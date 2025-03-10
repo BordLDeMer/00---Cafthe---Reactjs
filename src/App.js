6c1f8e90b8d5b665464a0b4194bc014885286e9a
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-03-10 16:52:05 +0100

    Yahla

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import TheInfusions from "./pages/TheInfusions";
 import CadeauxCoffrets from "./pages/CadeauxCoffrets";
 import Cafes from "./pages/Cafes";
 import Appareils from "./pages/Appareils";
-
+import Register from "./pages/register";
 
 function App() {
   return (
@@ -29,6 +29,8 @@ function App() {
             <Route path={"nos-thes-infusions"} element={<TheInfusions />}/>
             <Route path={"nos-machines"} element={<Appareils />}/>
             <Route path={"CadeauxCoffrets"} element={<CadeauxCoffrets />}/>
+            <Route path={"inscription"} element={<Register />}/>
+
           </Route>
         </Routes>
       </Router>
